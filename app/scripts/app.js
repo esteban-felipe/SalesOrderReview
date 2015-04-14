f85586a2f86b0b97f4c1e340a63586e2ea895de1
Eststeban Felipe <felipe@intalio.com>
2015-04-14 17:08:33 -0400

    Layout complete

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name salesOrderReviewApp
+ * @description
+ * # salesOrderReviewApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('salesOrderReviewApp', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name salesOrderReviewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the salesOrderReviewApp
+ */
+angular.module('salesOrderReviewApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
